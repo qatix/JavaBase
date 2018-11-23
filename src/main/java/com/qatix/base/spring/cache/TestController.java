207fb4f8acc77d2851f393c13eacc4fa1ff9e4fc
LoganTang <logan.tang@kikatech.com>
2018-11-23 15:30:56 +0800

    add spring cache

---
+++ src/main/java/com/qatix/base/spring/cache/TestController.java
@@ -0,0 +1,32 @@
+package com.qatix.base.spring.cache;
+
+/**
+ * @Author: Logan.Tang
+ * @Date: 2018/11/23 1:36 PM
+ */
+//@RestController
+//@RequestMapping("/cache")
+public class TestController {
+//
+//    @Cacheable(value = "testc", condition = "#id < 10", key = "#id")
+//    @GetMapping("/get/{id}")
+//    public String getCache(@PathVariable(name = "id") Integer id) {
+//        try {
+//            Thread.sleep(100);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        return String.format("id:%d:%d", id, System.currentTimeMillis());
+//    }
+//
+//    @CacheEvict(value = "testc4", condition = "#id < 10", key = "#id")
+//    @GetMapping("/put/{id}")
+//    public String delCache(@PathVariable(name = "id") Integer id) {
+//        try {
+//            Thread.sleep(100);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        return "OK";
+//    }
+}
