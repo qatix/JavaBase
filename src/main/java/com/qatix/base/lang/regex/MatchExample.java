07fbaf4f6d9feed1267081274e3785ff5f3fde55
Xiaojun Tang <tangxiaojun@checheweike.com>
2019-03-28 01:03:06 +0800

    update regex

---
+++ src/main/java/com/qatix/base/lang/regex/MatchExample.java
@@ -0,0 +1,25 @@
+package com.qatix.base.lang.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchExample {
+
+    public static void main(String[] args) {
+        System.out.println(match("^\\d+$","12312"));
+        System.out.println(match("^\\d+$","12312a"));
+        System.out.println(match("^[A-Za-z]+$","12312"));
+        System.out.println(match("^[A-Za-z]+$","aaa"));
+        System.out.println(match("^[A-Za-z]+$","12312bbb"));
+        System.out.println(match("[!@#\\$%]*","aa%#"));
+        System.out.println(match("[!@#\\$%]+","12-"));
+        System.out.println(match("[!@#\\$%]+","#$"));
+
+    }
+
+    public static boolean match(String regex,String str){
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+        return matcher.matches();
+    }
+}
