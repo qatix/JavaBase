5e8e4901622ed1201d6a64bc1f358bb6c4f96b86
Xiaojun Tang <tangxiaojun@checheweike.com>
2021-04-24 23:05:14 +0800

    test supplier

--- src/main/java/com/qatix/base/lang/java8/SupplierExample.java
+++ src/main/java/com/qatix/base/lang/java8/SupplierExample.java
@@ -10,7 +10,9 @@ public class SupplierExample {
     public static void main(String[] args) {
         //Supplier接口返回一个任意泛型的值，没有任何参数
         Supplier<Student> supplier = Student::new;
+        System.out.println("supplier initialized");
         Student student = supplier.get();
+        System.out.println("supplier get");
         System.out.println(student);
 
         student.setName("zhang");
@@ -22,6 +24,7 @@ public class SupplierExample {
 
         public Student() {
             name = "Name is not set";
+            System.out.println("construct");
         }
 
         public void setName(String name) {
